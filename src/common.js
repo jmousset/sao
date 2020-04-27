97ee22b1e9ce4c78fb21efbbb17fb917ce68d9e9
Cédric Krier <ced@b2ck.com>
2020-04-28 00:31:05 +0200

    Make overflow-y visible to display completion
    issue9231
    review293611002

--- src/common.js
+++ src/common.js
@@ -3335,6 +3335,7 @@
                     .css('max-height', '');
                 this.input.closest('.modal-body').css('overflow', '');
                 this.input.closest('.navbar-collapse.in').css('overflow-y', '');
+                this.input.closest('.content-box').css('overflow-y', '');
                 Sao.common.scrollIntoViewIfNeeded(this.input);
             }.bind(this));
             this.dropdown.on('show.bs.dropdown', function() {
@@ -3343,6 +3344,7 @@
                     .css('max-height', 'none');
                 this.input.closest('.modal-body').css('overflow', 'visible');
                 this.input.closest('.navbar-collapse.in').css('overflow-y', 'visible');
+                this.input.closest('.content-box').css('overflow-y', 'visible');
                 Sao.common.scrollIntoViewIfNeeded(this.input);
             }.bind(this));
         },
