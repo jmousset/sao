62ec876dc1ad59cdf1131e446f2a11d37d51ec9f
Sergi Almacellas Abellana <sergi@koolpi.com>
2020-06-03 21:02:49 +0200

    Allow html objects in autocompletion entries
    issue9385
    review317551002
    (grafted from 1a8fdd7bd1e1db7196e42d3e2a690aa4f162fa17)
    
    --HG--
    branch : 5.6

--- src/common.js
+++ src/common.js
@@ -3380,7 +3380,7 @@
             if (this.format) {
                 return this.format(content);
             }
-            return content;
+            return jQuery('<span/>').text(content);
         },
         _format_action: function(content) {
             if (this.format_action) {
@@ -3417,7 +3417,7 @@
                     'class': 'completion'
                 }).append(jQuery('<a/>', {
                     'href': '#'
-                }).text(this._format(value)))
+                }).append(this._format(value)))
                 .click(function(evt) {
                     evt.preventDefault();
                     if (this.match_selected) {
